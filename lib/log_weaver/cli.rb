8f8e5ba51c5bdc4644aebc686f453d6dc28f4ac5
raphael <raphael.borowiecki@gmail.com>
2012-10-11 06:38:29 -0400

    wip - get_file_prefixes

--- lib/log_weaver/cli.rb
+++ lib/log_weaver/cli.rb
@@ -4,8 +4,29 @@ include Methadone::Main
 
 module LogWeaver
   module CLI
-    def get_file_prefixes(file1, file2)
+    PREFIX_LENGTH_LIMIT = 4
 
+    # given 2 file names, generate file name prefixes given the following rules:
+    # 1. prefixes have to differ
+    # 2. prefixes have to be longer than PREFIX_LENGTH_LIMIT, unless file name is shorter
+    # 3. if both file names are shorter than PREFIX_LENGTH_LIMIT, grab whole directories from directory path
+    def get_file_prefixes(file1, file2)
+      files = [file1, file2].sort_by(&:length)
+
+      files = {}
+      [file1, file2].each{ |f| files[File.expand_path(f)] = File.basename(f) }
+
+      # pseudocode:
+      #
+      case file1.length <=> file2.length
+        when -1
+
+        when 1
+        when 0
+      end
     end
+
+
+
   end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,7 +7,8 @@ module LogWeaver
   module CLI
 
     describe "#get_file_prefixes" do
-      it "should  " do
+      it "should return whole file name if shorter than PREFIX_LENGTH_LIMIT and file name trimmed to PREFIX_LENGTH_LIMIT characters if longer than PREFIX_LENGTH_LIMIT" do
+        get_file_prefixes("fil", "file2").should == ["fil", "file"]
 
       end
 
