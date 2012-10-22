f0da3952868078de023ae679513ae1b0a2f125ae
raphael <raphael.borowiecki@gmail.com>
2012-10-21 21:48:26 -0400

    make cuke stop after first failure

--- features/step_definitions/log_weaver_steps.rb
+++ features/step_definitions/log_weaver_steps.rb
@@ -1,5 +1,11 @@
 require 'fileutils'
 
+if ENV['FAILFAST']
+  After do |s|
+    Cucumber.wants_to_quit = true if s.failed?
+  end
+end
+
 Given /^no file named "([^"]*)"$/ do |file_name|
   check_file_presence( [file_name], false )
 end
