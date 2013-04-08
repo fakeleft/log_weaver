dadd1debe09b4aa83dac33c5f2289fc0430c778d
raphael <raphael.borowiecki@gmail.com>
2013-04-08 15:42:16 -0400

    Copy some of the vars from parsed_log_spec into global vars so they're available in factory definitions.

---
+++ spec/common_variables.rb
@@ -0,0 +1,21 @@
+# Vars common to specs and factories; I tried wrapping them in a module, i.e.:
+# module CommonVariables
+#   def p1() 'p1' end
+# end
+# but having modules deep in Rspec get access to it by extending was way more voodoo
+# than I can stomach for now. So we'll go with globals.
+
+$p1 = 'p1'
+$p2 = 'p2'
+
+$t1 = Time.parse('2000-01-01 00:00:01.000') # NOTE: init time this way to discard values below msec
+$t2 = $t1 + 1
+
+$l1 = ' l1'
+$l2 = ' l2'
+
+$t1_l1 = "#{$t1} - t1 l1"
+$t1_l2 = "#{$t1} - t1 l2"
+$t2_l1 = "#{$t2} - t2 l1"
+$t2_l2 = "#{$t2} - t2 l2"
+$no_t_line = "no t"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,10 @@ require 'factory_girl'
 require 'factories'
 
 require 'log_weaver'
+require 'common_variables'
+
 
 RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
 end
-
 
