e66aa54a2af32e60ddd74b9e4e2ca4d05441f2bb
raphael <raphael.borowiecki@gmail.com>
2013-04-11 06:09:41 -0400

    Variables are now set up in factories_and_globals.

--- spec/combined_log_spec.rb
+++ spec/combined_log_spec.rb
@@ -5,33 +5,6 @@ module LogWeaver
   class CombinedLog
     describe "CombinedLog" do
       before(:all) do
-        @p1 = "prefix1:"
-        @p2 = "prefix2:"
-
-        @t1 = Time.parse("2000-01-01 00:00:01.000")
-        @t2 = @t1 + 1
-
-        @t1l1 = "#{@t1} - t1 l1"
-        @t1l2 = "#{@t1} - t1 l2"
-        @t2l1 = "#{@t2} - t2 l1"
-        @t2l2 = "#{@t2} - t2 l2"
-
-        @no_t_line = "no t"
-
-        @p1_t1l1_parsed = "#{@p1}#{@t1l1}"
-        @p2_t2l1_parsed = "#{@p2}#{@t2l1}"
-
-        @t1l1_log = StringIO.new(@t1l1)
-        @t2l1_log = StringIO.new(@t2l1)
-
-        @t1l1_t2l1_log = StringIO.new([@t1l1, @t2l1].join("\n"))
-        @t1l2_t2l2_log = StringIO.new([@t1l2, @t2l2].join("\n"))
-
-        @pl_p1_t1l1 = ParsedLog.new(@t1l1_log, @p1)
-        @pl_p2_t1l1 = ParsedLog.new(@t1l1_log, @p2)
-
-        @logs = [@pl_p1_t1l1, @pl_p2_t1l1]
-        @cl = CombinedLog.new(@logs)
       end
 
 
